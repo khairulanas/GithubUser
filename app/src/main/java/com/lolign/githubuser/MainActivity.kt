dd9e0a576fa9cefb34369663772ccb600b977ef5
khairul anas <mafiakanan@gmail.com>
2021-02-24 11:28:18 +0700

    home page

--- app/src/main/java/com/lolign/githubuser/MainActivity.kt
+++ app/src/main/java/com/lolign/githubuser/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.lolign.githubuser
 
+import android.content.res.TypedArray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: UserAdapter
+    private lateinit var dataUsername:Array<String>
+    private lateinit var dataName:Array<String>
+    private lateinit var dataPhoto:TypedArray
+
+    private lateinit var dataCompany:Array<String>
+    private lateinit var dataLocation:Array<String>
+    private lateinit var dataRepository:Array<String>
+    private lateinit var dataFollowers:Array<String>
+    private lateinit var dataFollowing:Array<String>
+    private var users = arrayListOf<User>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView:ListView = findViewById(R.id.lv_list)
+        adapter = UserAdapter(this)
+        listView.adapter = adapter
+
+        prepare()
+        addItem()
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            Toast.makeText(this, users[position].name, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun addItem() {
+        for (position in dataUsername.indices){
+            val user = User(
+                dataUsername[position],
+                dataName[position],
+                dataPhoto.getResourceId(position,-1),
+                dataCompany[position],
+                dataLocation[position],
+                dataRepository[position],
+                dataFollowers[position],
+                dataFollowing[position],
+            )
+            users.add(user)
+        }
+        adapter.users = users
+    }
+
+    private fun prepare() {
+        dataUsername = resources.getStringArray(R.array.username)
+        dataName = resources.getStringArray(R.array.name)
+        dataPhoto = resources.obtainTypedArray(R.array.avatar)
+        dataCompany = resources.getStringArray(R.array.company)
+        dataLocation= resources.getStringArray(R.array.location)
+        dataRepository= resources.getStringArray(R.array.repository)
+        dataFollowers= resources.getStringArray(R.array.followers)
+        dataFollowing= resources.getStringArray(R.array.following)
     }
 }
---
+++ app/src/main/java/com/lolign/githubuser/User.kt
@@ -0,0 +1,12 @@
+package com.lolign.githubuser
+
+data class User (
+    var username:String,
+    var name:String,
+    var avatar:Int,
+    var company:String,
+    var location:String,
+    var repository:String,
+    var followers:String,
+    var following:String,
+)

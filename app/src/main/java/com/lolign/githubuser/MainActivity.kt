9517b6d6091235640921b0242781b8fd6db8dcd8
khairul anas <mafiakanan@gmail.com>
2021-03-07 11:34:04 +0700

    add search view on appbar

--- app/src/main/java/com/lolign/githubuser/MainActivity.kt
+++ app/src/main/java/com/lolign/githubuser/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.lolign.githubuser
 
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.lolign.githubuser.databinding.ActivityMainBinding
 
@@ -23,6 +27,34 @@ class MainActivity : AppCompatActivity() {
 
         list.addAll(getListUsers())
         showRecyclerList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            /*
+            Gunakan method ini ketika search selesai atau OK
+             */
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            /*
+            Gunakan method ini untuk merespon tiap perubahan huruf pada searchView
+             */
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return true
     }
 
     private fun showSelectedUser(user: User) {
